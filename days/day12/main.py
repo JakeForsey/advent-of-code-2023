19af0502df7f0182f5996f2ee2fec1e030ccb6d1
Jake F <jakeforsey@googlemail.com>
2023-12-12 23:12:00 +0000

    day12 part2 python

--- days/day12/main.py
+++ days/day12/main.py
@@ -5,88 +5,30 @@ from collections import defaultdict
 with open("days/day12/input", "r") as f:
     data = f.read()
 
-def valid(pattern, counts):
-    contiguous = 0
-    for c in pattern:
-        if c == ".":
-            if contiguous > 0:
-                if not counts:
-                    return False
-                count = counts.pop(0)
-                if count != contiguous:
-                    return False
-            contiguous = 0
-        elif c == "#":
-            contiguous += 1
-        elif c == "?":
-            raise AssertionError("Unreachable")
-    
-    if contiguous > 0:
-        if not counts:
-            return False
-        count = counts.pop()
-        if count != contiguous:
-            return False
-    if counts:
-        return False
-    return True
-
-# result = 0
-# for line in enumerate(data.splitlines()):
-
-#     pattern, counts = line.split(" ")
-#     pattern = list(pattern)
-#     counts = list(map(int, counts.split(",")))
-    
-#     total = sum(counts)
-#     hashes = sum(1 for c in pattern if c == "#")
-#     for choice in set(combinations([i for i, c in enumerate(pattern) if c == "?"], total - hashes)):
-#         tmp = []
-#         for i, c in enumerate(pattern):
-#             if c == "?":
-#                 tmp.append("." if i not in choice else "#")
-#             else:
-#                 tmp.append(c)
-
-#         v = valid(tmp, counts.copy())
-#         if v:
-#             result += 1
-
-# print(f"part1: {result}")
-
-result = 0
-for li, line in enumerate(data.splitlines()):
-    print(line, end="")
-
+def process_line(line, folds=5):
     pattern, counts = line.split(" ")
-    pattern = list("?".join(pattern for _ in range(5)))
-    counts = ",".join(counts for _ in range(5))
-    counts = list(map(int, counts.split(",")))
+    pattern = list("?".join(pattern for _ in range(folds)))
+    counts = list(map(int, counts.split(","))) * folds
 
     options = defaultdict(list)
     for count in set(counts):
         for start in range(len(pattern)):
-            end = start + (count - 1)
+            end = start + count
             book_start = start - 1
-            book_end = end + 1
-            if end >= len(pattern):
+            if end > len(pattern):
                 continue
             if book_start >= 0 and pattern[book_start] not in "?.":
                 continue
-            if book_end < len(pattern) and pattern[book_end] not in "?.":
+            if end < len(pattern) and pattern[end] not in "?.":
                 continue
-            if "." in pattern[start: end + 1]:
+            if "." in pattern[start: end]:
                 continue
             options[count].append((start, end))
 
-    # print("options:")
-    # for count, option in options.items():
-    #     print(f"{count}: {option}")
-
     @lru_cache(maxsize=1_000_000)
-    def recursive(last, counts):
+    def recursive(last: int, counts: tuple[counts]) -> int:
         if not counts:
-            return 1
+            return 0 if "#" in pattern[last:] else 1
 
         try:
             next_hash = pattern.index("#", last + 1)
@@ -98,16 +40,19 @@ for li, line in enumerate(data.splitlines()):
             if start > next_hash:
                 break
             if start > last:
-                total += recursive(end + 1, counts[1:])
+                total += recursive(end, counts[1:])
         return total
 
-    line_result = recursive(-1, tuple(counts))
-    print(f" - {line_result} arrangement")
-    result += line_result
+    return recursive(-1, tuple(counts))
+
+result = 0
+for line in data.splitlines():
+    result += process_line(line, folds=1)
+
+print(f"part1: {result}")
+
+result = 0
+for line in data.splitlines():
+    result += process_line(line, folds=5)
 
 print(f"part2: {result}")
-
-# too low: 9377
-# too high: 20655489691690,
-#           34191380712181
-#           35024564475996
