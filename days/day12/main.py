2a98a6211e2f24c6d162f6f916797882361057a9
Jake F <jakeforsey@googlemail.com>
2023-12-12 22:44:22 +0000

    Cloose!

--- days/day12/main.py
+++ days/day12/main.py
@@ -1,4 +1,6 @@
+from functools import lru_cache
 from itertools import combinations, takewhile
+from collections import defaultdict
 
 with open("days/day12/input", "r") as f:
     data = f.read()
@@ -29,25 +31,83 @@ def valid(pattern, counts):
         return False
     return True
 
+# result = 0
+# for line in enumerate(data.splitlines()):
+
+#     pattern, counts = line.split(" ")
+#     pattern = list(pattern)
+#     counts = list(map(int, counts.split(",")))
+    
+#     total = sum(counts)
+#     hashes = sum(1 for c in pattern if c == "#")
+#     for choice in set(combinations([i for i, c in enumerate(pattern) if c == "?"], total - hashes)):
+#         tmp = []
+#         for i, c in enumerate(pattern):
+#             if c == "?":
+#                 tmp.append("." if i not in choice else "#")
+#             else:
+#                 tmp.append(c)
+
+#         v = valid(tmp, counts.copy())
+#         if v:
+#             result += 1
+
+# print(f"part1: {result}")
+
 result = 0
 for li, line in enumerate(data.splitlines()):
+    print(line, end="")
 
     pattern, counts = line.split(" ")
-    pattern = list(pattern)
+    pattern = list("?".join(pattern for _ in range(5)))
+    counts = ",".join(counts for _ in range(5))
     counts = list(map(int, counts.split(",")))
-    
-    total = sum(counts)
-    hashes = sum(1 for c in pattern if c == "#")
-    for choice in set(combinations([i for i, c in enumerate(pattern) if c == "?"], total - hashes)):
-        tmp = []
-        for i, c in enumerate(pattern):
-            if c == "?":
-                tmp.append("." if i not in choice else "#")
-            else:
-                tmp.append(c)
 
-        v = valid(tmp, counts.copy())
-        if v:
-            result += 1
+    options = defaultdict(list)
+    for count in set(counts):
+        for start in range(len(pattern)):
+            end = start + (count - 1)
+            book_start = start - 1
+            book_end = end + 1
+            if end >= len(pattern):
+                continue
+            if book_start >= 0 and pattern[book_start] not in "?.":
+                continue
+            if book_end < len(pattern) and pattern[book_end] not in "?.":
+                continue
+            if "." in pattern[start: end + 1]:
+                continue
+            options[count].append((start, end))
 
-print(f"part1: {result}")
+    # print("options:")
+    # for count, option in options.items():
+    #     print(f"{count}: {option}")
+
+    @lru_cache(maxsize=1_000_000)
+    def recursive(last, counts):
+        if not counts:
+            return 1
+
+        try:
+            next_hash = pattern.index("#", last + 1)
+        except ValueError:
+            next_hash = len(pattern)
+
+        total = 0
+        for (start, end) in options[counts[0]]:
+            if start > next_hash:
+                break
+            if start > last:
+                total += recursive(end + 1, counts[1:])
+        return total
+
+    line_result = recursive(-1, tuple(counts))
+    print(f" - {line_result} arrangement")
+    result += line_result
+
+print(f"part2: {result}")
+
+# too low: 9377
+# too high: 20655489691690,
+#           34191380712181
+#           35024564475996
