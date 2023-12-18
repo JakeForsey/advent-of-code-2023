c9b2f111f5c86da804f9695497fab739af3ae4cb
Jake F <jakeforsey@googlemail.com>
2023-12-18 19:03:46 +0000

    day18 part2 python

--- days/day18/main.py
+++ days/day18/main.py
@@ -1,61 +1,79 @@
 from collections import defaultdict
+from typing import Callable, NamedTuple
+
+DIRECTIONS_LIST = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+DIRECTIONS_MAP = {"R": (1, 0), "L": (-1, 0), "D": (0, 1), "U": (0,-1)}
+
+class Point(NamedTuple):
+    x: int
+    y: int
+
+class Line(NamedTuple):
+    start: Point
+    end: Point
+
+def ray_intersects(ray: Line, line: Line) -> bool:
+    if line.start.y == line.end.y:
+        return False  # line is horizontal (so cant intersect the horizontal ray)
+    if line.start.y > ray.start.y and line.end.y > ray.start.y:
+        return False  # line is below the ray
+    if line.start.y < ray.start.y and line.end.y < ray.start.y:
+        return False # line is above the ray
+    if line.end.x < ray.start.x:
+        return False
+    return True
+
+def run(decode: Callable[[str], tuple[tuple[int, int], int]]) -> int:
+    polygon_len = 0
+    polygon = []
+    last_x, last_y = (0, 0)
+    for line in lines:
+        (dx, dy), length = decode(line)
+        polygon.append((last_x, last_y))
+        dxl = dx * length
+        dyl = dy * length
+        polygon_len += abs(dxl) + abs(dyl)
+        last_x, last_y = (last_x + dxl, last_y + dyl)
+    polygon.append((0, 0))
+
+    xs = sorted(set([x for x, _ in polygon]))
+    ys = sorted(set([y for _, y in polygon]))
+    max_x = xs[-1] + 1
+
+    coords = defaultdict(lambda: None)
+    for y1, y2 in zip(ys[::1], ys[1::1]):
+        for x1, x2 in zip(xs[::1], xs[1::1]):
+            width = x2 - x1
+            height = y2 - y1
+            coords[(x1 + (width / 2), y1 + (height / 2))] = height * width
+
+
+    result = 0
+    for (x, y), count in coords.items():
+        n = 0
+        ray = Line(Point(x, y), Point(max_x, y))
+        for p1, p2 in zip(polygon[::1], polygon[1::1]):
+            if ray_intersects(ray, Line(Point(*p1), Point(*p2))):
+                n += 1
+        if n % 2 != 0:
+            result += count
+    
+    return result + (polygon_len //2) + 1
+
+def part2_decode(line):
+    _, __, colour = line.split(" ")
+    hex_str = colour.replace("(#", "").strip(")")
+    length = int("0x" + hex_str[:5], 16)
+    dx, dy = DIRECTIONS_LIST[int("0x" + hex_str[5], 16)]
+    return (dx, dy), length
+
+def part1_decode(line):
+    direction, length, _ = line.split(" ")
+    dx, dy = DIRECTIONS_MAP[direction]
+    return (dx, dy), int(length)
 
 with open("days/day18/input", "r") as f:
     lines = f.read().splitlines()
 
-DIRECTIONS = {
-    "R": (1, 0),
-    "L": (-1, 0),
-    "D": (0, 1),
-    "U": (0,-1),
-}
-position = (0, 0)
-coords = {position}
-for line in lines:
-    direction, length, colour = line.split(" ")
-    dx, dy = DIRECTIONS[direction]
-    for i in range(int(length)):
-        position = position[0] + dx, position[1] + dy
-        coords.add(position)
-
-min_x = min((x for x, _ in coords))
-min_y = min((y for _, y in coords))
-max_x = max((x for x, _ in coords))
-max_y = max((y for _, y in coords))
-areas = []
-for y in range(min_y, max_y):
-    for x in range(min_x, max_x):
-        if (x, y) in coords:
-            continue
-
-        if any((x, y) in area for area, _ in areas):
-            continue
-        
-        enclosed = True
-        todo = [(x, y)]
-        area = set()
-        while todo:
-            pos = todo.pop()
-            area.add(pos)
-            for direction in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                next_pos = pos[0] + direction[0], pos[1] + direction[1]
-                
-                if next_pos in area:
-                    continue
-                
-                if next_pos in coords:
-                    continue
-                    
-                if next_pos[0] < min_x or next_pos[0] >= max_x + 1:
-                    enclosed = False
-                    continue
-
-                if next_pos[1] < min_y or next_pos[1] >= max_y + 1:
-                    enclosed = False
-                    continue
-                
-                todo.append(next_pos)
-        areas.append((area, enclosed))
-
-area = [area for area, enclosed in areas if enclosed][0]
-print(f"part1: {len(area) + len(coords)}")
+print(f"part1: {run(part1_decode)}")
+print(f"part2: {run(part2_decode)}")
