b6ed51df20e87a1a172577a1dccd2e8706305ec7
Jake F <jakeforsey@googlemail.com>
2023-12-25 01:46:51 +0000

    day24 part2 python

--- days/day24/main.py
+++ days/day24/main.py
@@ -1,4 +1,6 @@
-with open("days/day24/input", "r") as f:
+from z3 import Solver, Ints, Int, sat
+
+with open("input", "r") as f:
     lines = f.read().splitlines()
 
 formulas = []
@@ -10,7 +12,7 @@ for line in lines:
     y1, y2 = py, py + vy
     m = (y2 - y1) / (x2 - x1)
     c = -m * x1 + y1
-    formulas.append((px, py, vx, vy, m, c))
+    formulas.append((px, py, pz, vx, vy, vz, m, c))
 
 min_x = min_y = 200000000000000
 max_x = max_y = 400000000000000
@@ -18,8 +20,8 @@ max_x = max_y = 400000000000000
 result = 0
 for i, formula_1 in enumerate(formulas):
     for formula_2 in formulas[i:]:
-        px1, py1, vx1, vy1, m1, c1 = formula_1
-        px2, py2, vx2, vy2, m2, c2 = formula_2
+        px1, py1, _, vx1, vy1, _, m1, c1 = formula_1
+        px2, py2, _, vx2, vy2, _, m2, c2 = formula_2
         if m1 == m2:
             # Parallel
             continue
@@ -40,3 +42,19 @@ for i, formula_1 in enumerate(formulas):
             result += 1
 
 print(f"part1: {result}")
+
+solver = Solver()
+
+px0, py0, pz0 = Ints("px0 py0 pz0")
+vx0, vy0, vz0 = Ints("vx0 vy0 vz0")
+
+for i, (px1, py1, pz1, vx1, vy1, vz1, _, __) in enumerate(formulas[:3]):
+    t = Int(f"t{i}")
+    solver.add(t > 0)
+    solver.add(px1 + t * vx1 == px0 + t * vx0)
+    solver.add(py1 + t * vy1 == py0 + t * vy0)
+    solver.add(pz1 + t * vz1 == pz0 + t * vz0)
+
+assert solver.check() == sat
+model = solver.model()
+print(f"part1: {model.eval(px0).as_long() + model.eval(py0).as_long() + model.eval(pz0).as_long()}")
